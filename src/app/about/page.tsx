0129f168b464d82079deac963fe9436a1cb4a3b5
M.Abdullah <alishafaqat4473@gmail.com>
2024-11-04 21:49:39 +0500

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+ import "../about/about.css"
+function About() {
+    return (  
+        <div>
+            <h1 className="headingabout">About Me</h1>
+            <div className="aboutpara">
+
+
+            Hello! I’m Muhammad Abdullah, a Class 10 student with a strong enthusiasm for technology and design. Currently, I am enrolled in the GIAIC course, where I’m learning the fundamentals of web development and enhancing my skills in programming languages like HTML, CSS, JavaScript, TypeScript, and Next.js. This portfolio is a collection of my projects that showcase my ability to merge creativity with technical knowledge, reflecting my journey as I strive to become a proficient developer. <br/> <br />
+            What excites me most is the opportunity to learn from others and share ideas. I enjoy participating in group projects and engaging in discussions that broaden my understanding of technology. I find that collaboration sparks creativity and leads to innovative solutions. Thank you for visiting my portfolio! I hope you find my work interesting, and I’m eager to receive any feedback that can help me grow in this dynamic field.
+             </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import "./page.css"
+function Home() {
+  return ( 
+    <div>
+      <h1 className="mainh">  Welcome To My Portfolio ! </h1>
+
+      <p className="mainpare">
+   I am a passionate developer specializing in creating innovative and user-friendly web applications. This platform showcases my diverse range of projects, including applications built with Java, TypeScript, HTML, and CSS. Each project reflects my dedication to delivering high-quality code and exceptional user experiences. Explore my work to see how I blend creativity and technical expertise to solve real-world problems, and feel free to reach out if you're interested in collaborating or have any inquiries. Thank you for visiting!
+
+
+      </p>
+      <div className="picture">
+      <img src="/picture.jpeg" alt=" img" /></div>
+    </div>
+   );
+}
+
+export default Home;
